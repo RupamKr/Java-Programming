eb1a5a6d38bc6440e1b5cd344cae60bc92c5cf54
RupamKr <hit18ee.rupam@gmail.com>
2024-05-11 12:21:36 +0530

    added two function for generate random number

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,12 +1,24 @@
 package Sorting;
 
+import java.util.Arrays;
+
 /**
  * QuickSort
  */
 public class QuickSort {
 
     public static void main(String[] arg) {
-        System.out.println("Quick Sort");
+
+        int array[] = new int[10];
+        for(int i=0; i<array.length; i++){
+            array[i] = SortingUtils.generateRandomNumber(101);
+        }
+        printArray(array);
+        
+    }
+
+    public static void printArray(int array[]){
+        System.out.println(Arrays.toString(array));
     }
 
 }
--- Sorting/SortingUtils.java
+++ Sorting/SortingUtils.java
@@ -1,10 +1,23 @@
 package Sorting;
 
+import java.util.Random;
+
 public class SortingUtils {
 
     public static void swap(int x, int y, int[] array) {
         int temp = array[x];
         array[x] = array[y];
         array[y] = temp;
+    }
+
+    public static int generateRandomNumberBetweenRange(int range1, int range2){
+        Random random = new Random();
+        return random.nextInt(range2-range1)+1;
+        //note random.nextInt(100)-> it means it will generate random number between 0-99, 100 will exclude from the range;
+    }
+
+    public static int generateRandomNumber(int number){
+        Random random = new Random();
+        return random.nextInt(number);
     }
 }
