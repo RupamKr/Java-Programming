8d0da46d5eaf9f6b9c75606abafca6476ea0ef17
RupamKr <hit18ee.rupam@gmail.com>
2024-05-24 18:26:50 +0530

    fix package name

--- Sorting/SortingUtils.java
+++ Sorting/SortingUtils.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class SortingUtils {
 
+    private static final String CHARACTER = "abcdefghijklmnopqrstuvwxyz";
+
     public static void swap(int x, int y, int[] array) {
         int temp = array[x];
         array[x] = array[y];
@@ -20,5 +22,14 @@ public class SortingUtils {
     public static int generateRandomNumber(int number) {
         Random random = new Random();
         return random.nextInt(number);
+    }
+
+    public static String generateRandomString(int lengthOfString) {
+        StringBuilder sb = new StringBuilder(lengthOfString);
+        for (int i = 0; i < lengthOfString; i++) {
+            int index = generateRandomNumber(CHARACTER.length());
+            sb.append(String.valueOf(CHARACTER.charAt(index)));
+        }
+        return String.valueOf(sb.toString().charAt(0)).toUpperCase()+sb.toString().substring(1);
     }
 }
--- binary_system/BinaryNumber.java
+++ binary_system/BinaryNumber.java
@@ -3,7 +3,45 @@ package binary_system;
 
 public class BinaryNumber {
 
+    public static int maximumUnsignedValueOf(int bits) {
+
+        int num = 0;
+        for (int i = 0; i < bits; i++) {
+            num = num + (int) Math.pow(2, i);
+        }
+        return num;
+    }
+
+    public static int maximumSignedValueOf(int bits) {
+        int num = 0;
+        for (int i = 0; i < bits - 1; i++) {
+            num = num + (int) Math.pow(2, i);
+        }
+        return num;
+    }
+
+    public static int minimumSignedValueOf(int bits) {
+        int num = 0;
+        for (int i = 0; i < bits - 1; i++) {
+            num = num + (int) Math.pow(2, i);
+        }
+        return -(num + 1);
+    }
+
     public static void main(String[] args) {
+
+        // byte byteInbyte = Byte.BYTES;
+        // byte byteInShort = Short.BYTES;
+        // byte byteInInt = Integer.BYTES;
+        // byte byteInLong = Long.BYTES;
+
+        // System.out.println("byteInbyte " + byteInbyte);
+        // System.out.println("byteInShort " + byteInShort);
+        // System.out.println("byteInInt " + byteInInt);
+        // System.out.println("byteInLong " + byteInLong);
+
+        //left shift and right shift
         
+
     }
 }
