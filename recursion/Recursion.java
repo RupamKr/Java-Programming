be064df1bd36e0454b4e04738937ee88fd7e7503
RupamKr <hit18ee.rupam@gmail.com>
2024-05-26 00:39:29 +0530

    working on recursion

---
+++ recursion/Recursion.java
@@ -0,0 +1,32 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import sorting.SortingUtils;
+
+public class Recursion {
+
+    public static ArrayList<String> subSequenceArray(int array[], int size) {
+
+        if (size == 0) {
+            ArrayList<String> bc = new ArrayList<>();
+            bc.add(" ");
+            return bc;
+        }
+
+        ArrayList<String> list = subSequenceArray(array, size - 1);
+
+        return list;
+    }
+
+    public static void main(String[] arg) {
+
+        int array[] = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = SortingUtils.generateRandomNumber(20);
+        }
+        ArrayList<String> result = subSequenceArray(array, array.length);
+        System.out.println(result);
+    }
+}
