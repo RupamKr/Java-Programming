0535d766f53df9d3c4f46a9442acb30e3aefb4c7
RupamKr <hit18ee.rupam@gmail.com>
2024-06-04 22:02:54 +0530

    write the logic for next greater element in an array

--- stack/StackDemo.java
+++ stack/StackDemo.java
@@ -1,14 +1,43 @@
 package stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
+import sorting.SortingUtils;
+
 public class StackDemo {
-    public static void main(String arg[]) {
-        //declare stack
+    public static int[] nextGreaterElement(int[] arr) {
+        int nge[] = new int[arr.length];
         Stack<Integer> st = new Stack<>();
-        //push integer into stack
-        st.push(10);
-        st.push(20);
-        System.out.println(st);
+        // we pushed index 0 to the stack
+        st.push(0);
+        // here we are moving from left to right;
+        for (int i = 1; i < arr.length; i++) {
+            // write the logic of pop;
+            //here if current element is greater then pop, until you find less element in stack
+            while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
+                int index = st.peek();
+                nge[index] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (!st.isEmpty()) {
+            int index = st.peek();
+            nge[index] = -1;
+            st.pop();
+        }
+        return nge;
+    }
+
+    public static void main(String arg[]) {
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = SortingUtils.generateRandomNumber(100);
+        }
+        
+        System.out.println(Arrays.toString(arr));
+        int ans[] = nextGreaterElement(arr);
+        System.out.println(Arrays.toString(ans));
     }
 }
