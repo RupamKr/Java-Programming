d44c181d2d00139689701dd2a487c4bec8ddaf79
RupamKr <hit18ee.rupam@gmail.com>
2024-06-04 21:35:21 +0530

    worked on stack

---
+++ rough_work/Main.java
@@ -0,0 +1,11 @@
+package rough_work;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String st = "harekrishna";
+        char[] ch = st.toCharArray();
+        System.out.println(Arrays.toString(ch));
+    }
+}
